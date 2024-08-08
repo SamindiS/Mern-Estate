1f0df6aec943727dcb26106c8a517ac31cd7c379
SamindiS <samindisankalpani@gmail.com>
2024-08-08 22:27:36 +0530

    Create user update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
     res.json({
         message: "User working!",
     })
+}
+
+export const updateUser = async(req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandlerHandler(403, "You can update only your account!"));
+        try {
+            if (req.body.password) {
+                req.body.password = bcryptjs.hashSync(req.body.password, 10);
+            }
+            const updateUser = await User.findByIdAndUpdate(req.params.id, {
+                    $set: {
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: req.body.password,
+                        avatar: req.body.avatar,
+                    },
+
+                }, { new: true }) //new: true returns the updated document
+            const { password: pass, ...rest } = updateUser._doc;
+            res.status(200).json(rest);
+
+        } catch (error) {
+            next(error);
+        }
+    }
 }
